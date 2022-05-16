42440c7b5c1b2e6809fa2316e5343d9c55370fe8
tuinderj <joshua.tuinder@gmail.com>
2022-05-16 12:35:42 -0600

    possibly fixed script issue

--- truck-data/Scripts/scripts.js
+++ truck-data/Scripts/scripts.js
@@ -1,3 +1,21 @@
+let data = {};
+
+$.get("database.csv", function(response, status) {
+  if(status=="success") {
+    data = $.csv.toObjects(response);
+    let html = '';
+    const body = document.querySelector("body");
+    for(let row in data) {
+      html += '<tr>\r\n';
+      for(let item in data[row]) {
+        html += '<td>' + data[row][item] + '</td>\r\n';
+      }
+      $('#contents').html(html);
+    }
+  }
+})
+
+
 let currentTruckIndex;
 
 function searchForTruck(searchCondition) {
